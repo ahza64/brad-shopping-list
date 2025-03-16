eb747fb6a3e50d9be6f7c6dfe4c8da94d6bc710d
ahza64 <justinmichaelpettit@gmail.com>
2025-03-15 23:15:00 -0700

    edit and save updated entries

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
 const itemFilter = document.getElementById('filter')
+const formBtn = itemForm.querySelector('button')
+let isEditMode = false
 
 function displayItems() {
   const itemsFromStorage = getItemsFromStorage()
@@ -16,6 +18,13 @@ function onAddItemSubmit(e) {
   if (newItem === '') {
     alert('please add an item')
     return
+  }
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector('.edit-mode')
+    removeItemFromStorage(itemToEdit.textContent)
+    itemToEdit.classList.remove('edit-mode')
+    itemToEdit.remove()
+    isEditMode = false
   }
 
   addItemToDOM(newItem)
@@ -68,7 +77,18 @@ function getItemsFromStorage(items) {
 function onClickItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
     removeItem(e.target.parentElement.parentElement)
+  } else {
+    setItemToEdit(e.target)
   }
+}
+
+function setItemToEdit(item) {
+  isEditMode = true
+  itemList.querySelectorAll('li').forEach((i) => i.classList.remove('edit-mode'))
+  item.classList.add('edit-mode')
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item'
+  formBtn.style.backgroundColor = "#228b22"
+  itemInput.value = item.textContent
 }
 
 function removeItem(item) {
@@ -107,6 +127,7 @@ function fitlerItems(e) {
 }
 
 function checkUI(e) {
+  itemInput.value = ''
   const items = itemList.querySelectorAll('li')
   if (items.length === 0) {
     clearBtn.style.display = 'none'
@@ -115,6 +136,10 @@ function checkUI(e) {
     clearBtn.style.display = 'block'
     itemFilter.style.display = 'block'
   }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item'
+  formBtn.style.backgroundColor = '#333'
+  isEditMode = false
 }
 
 function init() {
